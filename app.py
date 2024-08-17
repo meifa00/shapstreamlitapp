6fa0e3b4c1e00a4e44f692382da98a124d469c5d
meifa00 <73323559+meifa00@users.noreply.github.com>
2024-08-17 11:52:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ import matplotlib.pyplot as plt
 from streamlit_shap import st_shap
 
 # Load the dataset (assuming it's in the same directory)
-customer = pd.read_csv("Customer Churn.csv")
+@st.cache
+def load_data():
+    return pd.read_csv("Customer Churn.csv")
+
+customer = load_data()
 
 # Preprocessing
 X = customer.drop("Churn", axis=1)
@@ -34,7 +38,7 @@ st.title("SHAP Analysis for Customer Churn")
 
 # Part 1: General SHAP Analysis
 st.header("Part 1: General SHAP Analysis")
-st.dataframe(classification_report(y_pred, y_test,output_dict=True))
+st.dataframe(pd.DataFrame(classification_report(y_test, y_pred, output_dict=True)).T)
 
 # Summary plot
 st.subheader("Summary Plot")
@@ -54,12 +58,11 @@ st.header("Part 2: Individual Input Prediction & Explanation")
 # Input fields for features
 input_data = {}
 for feature in X.columns:
-    if feature in ['Call  Failure', 'Complains', 'Subscription  Length', 'Status' 'Seconds of Use',
+    if feature in ['Call Failure', 'Complains', 'Subscription Length', 'Status', 'Seconds of Use',
                    'Frequency of use', 'Frequency of SMS', 'Distinct Called Numbers', 'Age Group', 'Age']:
-        input_data[feature] = st.number_input(f"Enter {feature}:", value=int(X_test[feature].mean()), step=1)
+        input_data[feature] = st.number_input(f"Enter {feature}:", value=float(X_test[feature].mean()), step=1)
     else:  # For other features, keep the original input type
-        input_data[feature] = st.number_input(f"Enter {feature}:", value=X_test[feature].mean())
-
+        input_data[feature] = st.number_input(f"Enter {feature}:", value=float(X_test[feature].mean()))
 
 # Create a DataFrame from input data
 input_df = pd.DataFrame(input_data, index=[0])
@@ -75,19 +78,14 @@ st.write(f"**Churn Probability:** {probability:.2f}")
 # SHAP explanation for the input
 shap_values_input = explainer.shap_values(input_df)
 
-
 # Force plot
 st.subheader("Force Plot")
-# fig, ax = plt.subplots()
-# shap.plots.force(explainer.expected_value[0], shap_values_input[0,:], input_df.iloc[0,:], matplotlib=True)
-st_shap(shap.force_plot(explainer.expected_value[0], shap_values_input[0], input_df), height=400, width=1000)
-
-# st.write(input_df)
-# st.pyplot(fig,bbox_inches='tight')
+# Use st_shap for SHAP force plot
+fig = shap.force_plot(explainer.expected_value[0], shap_values_input[0], input_df)
+st_shap(fig)
 
 # Decision plot
 st.subheader("Decision Plot")
-# fig, ax = plt.subplots()
-# shap.decision_plot(explainer.expected_value[0], shap_values_input[0], X_test.columns)
-st_shap(shap.decision_plot(explainer.expected_value[0], shap_values_input[0], X_test.columns))
-# st.pyplot(fig)
+# Use st_shap for SHAP decision plot
+fig = shap.decision_plot(explainer.expected_value[0], shap_values_input[0], X_test.columns)
+st_shap(fig)
